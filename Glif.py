217805c00e3c6fc7e0b58ed32fd7a5ba4b605085
techzone3228 <techzone3228@gmail.com>
2025-07-14 09:56:44 +0500

    Update Glif.py

--- Glif.py
+++ Glif.py
@@ -843,7 +843,3 @@ if __name__ == '__main__':
     ============================================
     """)
     serve(app, host='0.0.0.0', port=8000)
-
-
-
-note this script and wait for my next instructions 
